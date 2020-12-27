1b1896faedf6c7497e229df434648318b7e5f2ee
unknown <Dayamar@Dayamar-PC>
2020-12-26 21:29:55 -0300

    nuevos caambios

--- empow-capitals/src/components/video.jsx
+++ empow-capitals/src/components/video.jsx
@@ -6,8 +6,7 @@ export default function video(){
         
         <div className='bgvideo'>
              <div className="video">
-                <iframe id="player" frameBorder="0" allowFullScreen="1" allow="accelerometer; autoplay; clipboard-write; 
-                encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="100%" height="100%"
+                <iframe id="player" frameBorder="0" allowFullScreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="100%" height="100%"
                 src="https://www.youtube.com/embed/xE5ZdFEnHgk?autoplay=1&enablejsapi=1"
                 data-gtm-yt-inspected-1_25="true">
                 </iframe>
